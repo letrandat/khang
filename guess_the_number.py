49bbb6c7b5b10af48bd15614bd0a57563318a62b
Tran Dat, Le <trandatnh@gmail.com>
2024-11-24 11:25:29 -0800

    Just Print a string

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,30 +1,5 @@
-import random
-
 def guess_the_number():
     """Simple number guessing game."""
 
-    number = random.randint(1, 100)
-    attempts = 0
-
-    print("Welcome to Guess the Number!")
-    print("I'm thinking of a number between 1 and 100.")
-
-    while True:
-        try:
-            guess = int(input("Enter your guess: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        attempts += 1
-
-        if guess < number:
-            print("Too low!")
-        elif guess > number:
-            print("Too high!")
-        else:
-            print(f"Congratulations! You guessed the number in {attempts} attempts.")
-            break
-
 if __name__ == "__main__":
-    guess_the_number()
+    print("A simple number guessing game.")
