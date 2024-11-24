76dfb994feb61609ab1fcdbaa428932090087e17
Tran Dat, Le <trandatnh@gmail.com>
2024-11-24 11:37:39 -0800

    roughly working version of the game

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,5 +3,22 @@ def guess_the_number():
     print("Welcome to Guess the Number!")
     print("I'm thinking of a number between 1 and 100.")
 
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        attempts += 1
+
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
 if __name__ == "__main__":
     guess_the_number()
